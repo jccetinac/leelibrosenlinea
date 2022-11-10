55e8b4ddd673ab19d629faa392ecf28331b64fe9
jccetinac <37853377+jccetinac@users.noreply.github.com>
2022-11-09 19:50:53 -0500

    cleancode

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -8,35 +8,21 @@ const Epub = ({ setSize, size }) => {
   return (
     <>
       <div className="settings">
-        <button
-          className="icon-btn"
-          onClick={() => setSettingOpen(!settingsOpen)}
-        >
-          <HiOutlineCog
-            color={settingsOpen ? 'orange' : 'grey'}
-            fontSize="1.4rem"
-          />
-        </button>
-        <div
-          id="settings_container"
-          style={{ display: settingsOpen ? 'flex' : 'none' }}
-        >
-          <p className="small-title">Font Size</p>
-          <div className="fontsize_change_div">
-            <button
-              className="icon-btn"
-              onClick={() => setSize(Math.max(70, size - 10))}
-            >
-              <HiChevronLeft color="grey" fontSize="1.4rem" />
-            </button>
-            <span className="size">{size}%</span>
-            <button
-              className="icon-btn"
-              onClick={() => setSize(Math.min(200, size + 10))}
-            >
-              <HiChevronRight color="grey" fontSize="1.4rem" />
-            </button>
-          </div>
+        <p className="small-title">Font Size</p>
+        <div className="fontsize_change_div">
+          <button
+            className="icon-btn"
+            onClick={() => setSize(Math.max(70, size - 10))}
+          >
+            <HiChevronLeft color="grey" fontSize="1.4rem" />
+          </button>
+          <span className="size">{size}%</span>
+          <button
+            className="icon-btn"
+            onClick={() => setSize(Math.min(200, size + 10))}
+          >
+            <HiChevronRight color="grey" fontSize="1.4rem" />
+          </button>
         </div>
       </div>
     </>
