b09f6c63c9c74ba9b0375f65429e68df951ff7ce
jccetinac <37853377+jccetinac@users.noreply.github.com>
2022-11-13 09:44:20 -0500

    libreria sin links

--- src/pages/BookStore.js
+++ src/pages/BookStore.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import DATA from '../Data/Data';
+import { Link } from 'react-router-dom';
 
-const BookStore = ({ mobile }) => {
-  return <>books Store</>;
+const BookStore = () => {
+  const UrlBase = 'https://zeektenka.github.io/files/';
+
+  return (
+    <div className="list-books">
+      {DATA.map((item) => {
+        return <Link to={`${UrlBase}${item.uri}`}>{item.name}</Link>;
+      })}
+    </div>
+  );
 };
 
 export default BookStore;
