ca10209aa179ad3330077be5e2ff4c8d4ca0c95a
jccetinac <37853377+jccetinac@users.noreply.github.com>
2022-11-14 18:41:42 -0500

    rememberLocation

--- src/Components/Epub.js
+++ src/Components/Epub.js
@@ -15,9 +15,7 @@ const Epub = ({ uri }) => {
   );
 
   const locationChanged = (epubcifi) => {
-    // epubcifi is a internal string used by epubjs to point to a location in an epub. It looks like this: epubcfi(/6/6[titlepage]!/4/2/12[pgepubid00003]/3:0)
     setLocation(epubcifi);
-    console.log(epubcifi);
     localStorage.setItem('CurrentPage', JSON.stringify(location));
   };
 
