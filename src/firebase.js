d25605045d93a2dcfca0ae6aea637544f00af003
LauGalviz <galvizlaura69@gmail.com>
2023-01-23 22:59:07 -0500

    firebase setting

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBfOH1mFvvoA5ArNHTAPXa8OZTyjIJiyTc',
-  authDomain: 'librosenlinea2022-50c14.firebaseapp.com',
-  projectId: 'librosenlinea2022-50c14',
-  storageBucket: 'librosenlinea2022-50c14.appspot.com',
-  messagingSenderId: '143652946405',
-  appId: '1:143652946405:web:f779fa2a3a7e852781ef0c',
-  measurementId: 'G-KGBL7NCQG2',
+  apiKey: "AIzaSyBfOH1mFvvoA5ArNHTAPXa8OZTyjIJiyTc",
+  authDomain: "librosenlinea2022-50c14.firebaseapp.com",
+  projectId: "librosenlinea2022-50c14",
+  storageBucket: "librosenlinea2022-50c14.appspot.com",
+  messagingSenderId: "143652946405",
+  appId: "1:143652946405:web:f779fa2a3a7e852781ef0c",
+  measurementId: "G-KGBL7NCQG2"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
